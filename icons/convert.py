96f5afb631bba243001b6b8298127b2d7774ee10
kkhhaalil <119089486+Cryptok777@users.noreply.github.com>
2024-09-23 14:05:34 -0700

    rename

---
+++ icons/convert.py
@@ -0,0 +1,15 @@
+import os
+
+
+def rename_images(directory):
+    for filename in os.listdir(directory):
+        if filename.endswith((".png", ".jpeg", ".gif", ".bmp", ".tiff")):
+            new_filename = os.path.splitext(filename)[0] + ".jpg"
+            os.rename(
+                os.path.join(directory, filename),
+                os.path.join(directory, new_filename),
+            )
+            print(f"Renamed: {filename} to {new_filename}")
+
+
+rename_images("./")
