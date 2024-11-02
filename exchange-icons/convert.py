6326a06d69fc1c135282368e8f0d0a531033822c
kkhhaalil <119089486+Cryptok777@users.noreply.github.com>
2024-11-02 03:57:33 -0700

    Update convert.py

--- exchange-icons/convert.py
+++ exchange-icons/convert.py
@@ -4,7 +4,7 @@ import os
 def rename_images(directory):
     for filename in os.listdir(directory):
         if filename.endswith((".png", ".jpeg", ".gif", ".bmp", ".tiff")):
-            new_filename = (os.path.splitext(filename)[0]).upper() + ".jpg"
+            new_filename = (os.path.splitext(filename)[0]).lower() + ".jpg"
             os.rename(
                 os.path.join(directory, filename),
                 os.path.join(directory, new_filename),
